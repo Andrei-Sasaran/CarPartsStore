b8884d98aca264ff9effaa815f2d78dacc769333
Tunol3 <i.cindea8@gmail.com>
2024-05-31 00:19:34 +0300

    cv la bazele de date

---
+++ SQLScripts/ii/engine/populare engine.sql
@@ -0,0 +1,62 @@
+USE [CarPartsStore]
+GO
+
+INSERT INTO [dbo].[Engine]([id],[name],[manufacturer],[model],[year],[type],[capacity],[cylinderNumber],[cylinderPlacement],[description],[stock],[price])
+VALUES(1, 'Ford Ranger 3.0L', 'Engines.com', '3.0L', 2004, 'Reman', 2999, 8, 'v8', 
+       'Our Engines.com Remanufactured 3.0L Ford Engine is the ultimate solution for your Ford Ranger lineup, covering model years 2004, 2005, 2006, 2007, and 2008. 
+	    Crafted with precision and expertise, this engine undergoes a meticulous remanufacturing process, breathing new life into your truck with every installation.', 2, 20050)
+GO
+
+INSERT INTO [dbo].[Engine]([id],[name],[manufacturer],[model],[year],[type],[capacity],[cylinderNumber],[cylinderPlacement],[description],[stock],[price])
+VALUES(2, 'Ford 5.4L 3Valve', 'Engines.com', '5.4L', 2004, 'Reman', 5399, 8, 'v8', 
+       'Our Engines.com Remanufactured 5.4L 3V Ford Engine is the ultimate solution for your Ford Expedition, Navigator, F-150, and more, covering model years 2004, 2005, and 2006. 
+	    Crafted with precision and expertise, this engine undergoes a meticulous remanufacturing process, breathing new life into your vehicle with every installation.', 2, 15000)
+GO
+
+INSERT INTO [dbo].[Engine]([id],[name],[manufacturer],[model],[year],[type],[capacity],[cylinderNumber],[cylinderPlacement],[description],[stock],[price])
+VALUES(3, 'Ford 5.4L 3Valve', 'Engines.com', '5.4L', 2008, 'Reman', 5399, 8, 'v8', 
+       'Our Engines.com Remanufactured 5.4L 3V Ford Engine is the ultimate solution for your Ford Expedition, Navigator, F-150, and more, covering model years 2004, 2005, and 2006. 
+	    Crafted with precision and expertise, this engine undergoes a meticulous remanufacturing process, breathing new life into your vehicle with every installation.', 2, 17750)
+GO
+
+INSERT INTO [dbo].[Engine]([id],[name],[manufacturer],[model],[year],[type],[capacity],[cylinderNumber],[cylinderPlacement],[description],[stock],[price])
+VALUES(4, 'Ford 4.2L', 'Engines.com', '5.4L', 2001, 'Reman', 4199, 6, 'v6', 
+       'Our Engines.com Remanufactured 4.2L Ford Engine is the ultimate solution for your Ford Econoline vans and trucks, covering model years 2001, 2002, 2003, and 2004. 
+	    Crafted with precision and expertise, this engine undergoes a meticulous remanufacturing process, breathing new life into your vehicle with every installation.', 2, 12000)
+GO
+
+INSERT INTO [dbo].[Engine]([id],[name],[manufacturer],[model],[year],[type],[capacity],[cylinderNumber],[cylinderPlacement],[description],[stock],[price])
+VALUES(5, 'Ford 4.0L', 'Engines.com', '4.0L', 2001, 'Reman', 3999, 8, 'v8', 
+       'Our Engines.com Remanufactured 4.0L Ford Engine is the ultimate solution for your Ford Econoline vans and trucks. 
+	    Crafted with precision and expertise, this engine undergoes a meticulous remanufacturing process, breathing new life into your vehicle with every installation.', 2, 14000)
+GO
+
+INSERT INTO [dbo].[Engine]([id],[name],[manufacturer],[model],[year],[type],[capacity],[cylinderNumber],[cylinderPlacement],[description],[stock],[price])
+VALUES(6, 'Ford 3.0L', 'Engines.com', '3.0L', 2000, 'Reman', 2999, 6, 'v6', 
+       'Our Engines.com Remanufactured 3.0L Ford Engine is the ultimate solution for your OEM Engine issues, this engine
+	    undergoes a meticulous remanufacturing process, breathing new life into your ride with every installation, whether its for your 1999, 2000, or 2001 vehicle.', 2, 14000)
+GO
+
+INSERT INTO [dbo].[Engine]([id],[name],[manufacturer],[model],[year],[type],[capacity],[cylinderNumber],[cylinderPlacement],[description],[stock],[price])
+VALUES(7, 'Dodge/Ram 6.4L', 'Engines.com', '6.4L', 2017, 'Reman', 6399, 8, 'v8', 
+       'Our Engines.com Remanufactured 6.4L Dodge Engine is the ultimate solution for your OEM Engine issues. This engine undergoes a meticulous remanufacturing process, breathing new life 
+	    into your ride with every installation, whether its for your 2013, 2014, 2015, 2016, or 2017 Ram 1500, 2500, or even a custom project!', 2, 30000)
+GO
+
+INSERT INTO [dbo].[Engine]([id],[name],[manufacturer],[model],[year],[type],[capacity],[cylinderNumber],[cylinderPlacement],[description],[stock],[price])
+VALUES(8, 'Dodge Ram 5.7L', 'Engines.com', '5.7L', 2005, 'Reman', 5699, 8, 'v8', 
+       'Our Engines.com Remanufactured 5.7L Dodge Engine is the ultimate solution for your OEM Engine issues. This engine undergoes a meticulous remanufacturing process, 
+	    breathing new life into your ride with every installation, whether its for your 2005, 2006, or 2007 Ram 1500.', 2, 20000)
+GO
+
+INSERT INTO [dbo].[Engine]([id],[name],[manufacturer],[model],[year],[type],[capacity],[cylinderNumber],[cylinderPlacement],[description],[stock],[price])
+VALUES(9, 'Dodge 3.6L', 'Engines.com', '3.6L', 2020, 'Reman', 3599, 6, 'v6', 
+       'Our Engines.com Remanufactured 3.6L Dodge Engine is the ultimate solution for your OEM Engine issues. This engine undergoes a meticulous remanufacturing process, 
+	   breathing new life into your ride with every installation, whether its for your 2019, 2020, or 2021 Dodge Ram and more.', 2, 23300)
+GO
+
+INSERT INTO [dbo].[Engine]([id],[name],[manufacturer],[model],[year],[type],[capacity],[cylinderNumber],[cylinderPlacement],[description],[stock],[price])
+VALUES(10, 'Hyundai 2.4L', 'Engines.com', '2.4L', 2020, 'Reman', 2399, 4, 'i4', 
+       'Our Engines.com Remanufactured 2.4L GM/Chevy Engine is the ultimate solution for your Hyundai Sonata lineup, including the popular Sonata model.
+	    Crafted with precision and expertise, this engine undergoes a meticulous remanufacturing process, breathing new life into your ride with every installation.', 2, 18640)
+GO
