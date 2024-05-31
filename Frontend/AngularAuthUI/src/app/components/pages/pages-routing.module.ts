77184fecd77d12d29d867ec49d7c1f8f8a198d3a
Sasaran Andrei <sasaranandreimihnea@gmail.com>
2024-05-31 21:36:09 +0300

    reparat Signup

--- Frontend/AngularAuthUI/src/app/components/pages/pages-routing.module.ts
+++ Frontend/AngularAuthUI/src/app/components/pages/pages-routing.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [{
   path: "dashboard",
