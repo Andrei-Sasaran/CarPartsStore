8bcdf2d38925bfc58a1fab3f42d51aaa1944621b
Sasaran Andrei <sasaranandreimihnea@gmail.com>
2024-06-02 02:22:49 +0300

    Account now knows who you are!

--- Frontend/AngularAuthUI/src/app/components/pages/account/account.component.ts
+++ Frontend/AngularAuthUI/src/app/components/pages/account/account.component.ts
@@ -1,25 +1,39 @@
 import { Component } from '@angular/core';
 import { PagesService } from '../pages.service';
 import { Router } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule, FormsModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
 export class AccountComponent {
 
   email:string='';
+  user: User;
+  userToSend:UserToSend;
   
-  constructor(private pagesService:PagesService, private router:Router){
+  constructor(private pagesService:PagesService, private http:HttpClient, private router:Router){
     this.pagesService.getEmail.subscribe(e => this.email = e);;
+    this.user = new User();
+    this.userToSend = new UserToSend();
   }
 
   ngOnInit() {
-    
+    this.userToSend.email = this.email;
+    this.http.post('http://localhost:57468/api/Users/GetUserByEmail', this.userToSend).subscribe((data:any) => {
+      this.user.username = data[0].userName;
+      this.user.shoppingCart = data[0].shoppingCart;
+      this.user.price = data[0].totalPrice;
+    })
   }
+
+
 
   toDashboard() {
     this.pagesService.setEmail(this.email);
@@ -36,4 +50,24 @@ export class AccountComponent {
     this.router.navigateByUrl('/vin');
   }
 
+}
+
+export class User {
+  email:string;
+  username:string;
+  shoppingCart:string;
+  price:number;
+  constructor(){
+    this.email = '';
+    this.username = '';
+    this.shoppingCart = '';
+    this.price = 0;
+  }
+}
+
+export class UserToSend {
+  email:string;
+  constructor() {
+    this.email ='';
+  }
 }
