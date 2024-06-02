f737ceff4f5dcbf94539badb19d75472de0a6137
Tunol3 <i.cindea8@gmail.com>
2024-06-02 17:22:59 +0300

    ShoppingCart Controller v1

---
+++ Backend/CarPartsStore/CarPartsStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CarPartsStore.Models;
+using static CarPartsStore.Controllers.CarsController;
+
+namespace CarPartsStore.Controllers
+{
+    public class ShoppingCartController : ApiController
+    {
+        [Route("api/ShoppingCart/CreateShoppingCart")]
+        [HttpPost]
+        public string CreateShoppingCart(ShoppingCart shop)
+        {
+            try
+            {
+                string query = @"
+                        Create table ["+ shop.tableName +@"]
+                        (
+                        [pieceCell] nvarchar(100),
+                        [pieceId] int
+                        )";
+                DataTable dataTable = new DataTable();
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["CarPartsStoreDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, connection))
+                using (var dataAdapter = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    dataAdapter.Fill(dataTable);
+                }
+                return "Created with succes!";
+            }
+            catch (Exception)
+            {
+                return "Creation failed!";
+            }
+        }
+        [Route("api/ShoppingCart/DeleteShoppingCart")]
+        [HttpPost]
+        public string DeleteShoppingCart(ShoppingCart shop)
+        {
+            try
+            {
+                string query = @"
+                        Drop table " + shop.tableName + @"";
+                DataTable dataTable = new DataTable();
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["CarPartsStoreDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, connection))
+                using (var dataAdapter = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    dataAdapter.Fill(dataTable);
+                }
+                return "Deleted with succes!";
+            }
+            catch (Exception)
+            {
+                return "Delete failed!";
+            }
+        }
+        public HttpResponseMessage Get(ShoppingCart shop)
+        {
+            string query = @"
+                    select * from "+ shop.tableName +@"
+                    ";
+            DataTable dataTable = new DataTable();
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["CarPartsStoreDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, connection))
+            using (var dataAdapter = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                dataAdapter.Fill(dataTable);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, dataTable);
+        }
+        [Route("api/ShoppingCart/AddToShoppingCart")]
+        [HttpPost]
+        public string AddToShoppingCart(ShoppingCart shop)
+        {
+            try
+            {
+                string query = @"
+                        insert into ["+ shop.tableName+@"] values
+                        (
+                        '" + shop.pieceCell + @"',
+                        '" + shop.pieceId + @"'
+                        )";
+                DataTable dataTable = new DataTable();
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["CarPartsStoreDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, connection))
+                using (var dataAdapter = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    dataAdapter.Fill(dataTable);
+                }
+                return "Inserted with succes!";
+            }
+            catch (Exception)
+            {
+                return "Insert failed!";
+            }
+        }
+    }
+}
---
+++ Backend/CarPartsStore/CarPartsStore/Models/ShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarPartsStore.Models
+{
+    public class ShoppingCart
+    {
+        public string tableName { get; set; }
+        public string pieceCell { get; set; }
+        public string pieceId { get; set; }
+    }
+}
